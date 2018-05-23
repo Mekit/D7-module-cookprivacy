d1cc934ec5660c3ca8b774f5cab50e2009f9ea4f
jakabadambalazs <jakabadambalazs@gmail.com>
2018-05-23 12:26:50 +0200

    code

---
+++ cp-cookie-old.tpl.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Cookie page
+ */
+?>
+
+<div class="container">
+  <div class="wrapper-cookie-page margin-b-4">
+    <div class="row">
+      <div class="col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
+        <h1>Cookie policy</h1>
+        <p>
+          La presente Cookie policy è relativa al sito <strong><?php print render($website); ?></strong> gestito e operato dall'azienda <strong><?php print render($website); ?></strong>.
+        </p>
+        <p>
+          I cookie sono piccoli file di testo inviati all'utente dal sito web visitato. Vengono memorizzati sull'hard disk del computer, consentendo in questo modo al sito web di riconoscere gli utenti e memorizzare determinate informazioni su di loro, al fine di permettere o migliorare il servizio offerto.
+        </p>
+        <p>
+          Esistono diverse tipologie di cookie. Alcuni sono necessari per poter navigare sul sito <strong><?php print render($website); ?></strong>, altri hanno scopi diversi come garantire la sicurezza interna, amministrare il sistema, effettuare analisi statistiche, comprendere quali sono le sezioni del sito che interessano maggiormente agli utenti o offrire una visita personalizzata del sito.
+        </p>
+        <p>
+          Il sito <strong><?php print render($website); ?></strong> utilizza <strong>cookie tecnici</strong> e non di profilazione. Quanto precede si riferisce sia al computer dell'utente sia ad ogni altro dispositivo che l'utente può utilizzare per connettersi al sito.
+        </p>
+
+        <h3>Cookie tecnici</h3>
+        <p>
+        I cookie tecnici sono quelli utilizzati al solo fine di effettuare la trasmissione di una comunicazione su una rete di comunicazione elettronica, o nella misura strettamente necessaria al fornitore di un servizio della società dell'informazione esplicitamente richiesto dall'abbonato o dall'utente a erogare tale servizio. Essi non vengono utilizzati per scopi ulteriori e sono normalmente installati direttamente dal titolare o gestore del sito web.</p>
+        <p>Possono essere suddivisi in <strong>cookie di navigazione o di sessione</strong>, che garantiscono la normale navigazione e fruizione del sito web (permettendo, ad esempio, di realizzare un acquisto o autenticarsi per accedere ad aree riservate); <strong>cookie analytics</strong>, assimilati ai cookie tecnici laddove utilizzati direttamente dal gestore del sito per raccogliere informazioni, in forma aggregata, sul numero degli utenti e su come questi visitano il sito stesso; <strong>cookie di funzionalità</strong>, che permettono all'utente la navigazione in funzione di una serie di criteri selezionati (ad esempio, la lingua, i prodotti selezionati per l'acquisto) al fine di migliorare il servizio reso allo stesso. La disabilitazione dei cookie potrebbe limitare la possibilità di usare il sito e impedire di beneficiare in pieno delle funzionalità e dei servizi presenti.
+        </p>
+
+        <h2>
+          Tipologie di cookie utilizzati dal sito <?php print render($website); ?>
+        </h2>
+        <h3>Cookie di prima parte</h3>
+        <p>
+          I cookie di prima parte (ovvero i cookies che appartengono al sito dell'editore che li ha creati) sono impostati dal sito web visitato dall'utente, il cui indirizzo compare nella finestra URL. L'utilizzo di tali cookie ci permette di far funzionare il sito in modo efficiente e di tracciare i modelli di comportamento dei visitatori.
+        </p>
+        <h3>Cookie di terzi</h3>
+        <p>
+          I cookie di terzi sono impostati da un dominio differente da quello visitato dall'utente. Se un utente visita un sito e una società diversa invia l'informazione sfruttando quel sito, si è in presenza di cookies di terze parti.
+        </p>
+        <h3>Cookie di sessione</h3>
+        <p>
+          I <em>cookies di sessione</em> sono memorizzati temporaneamente e vengono cancellati quando l'utente chiude il browser. Se l'utente si registra al sito, può utilizzare cookie che raccolgono dati personali al fine di identificare l'utente in occasione di visite successive e di facilitare l'accesso - login al sito (per esempio conservando username e password dell'utente) e la navigazione sullo stesso.<br/>
+          Il sito potrebbe contenere link ad altri siti. L'azienda <strong><?php print render($company); ?></strong> non ha alcun accesso o controllo su cookie, web bacon e altre tecnologie di tracciamento usate sui siti di terzi cui l'utente può accedere dal sito, sulla disponibilità, su qualsiasi contenuto e materiale che è pubblicato o ottenuto attraverso tali siti e sulle relative modalità di trattamento dei dati personali; l'azienda <strong><?php print render($company); ?></strong> a questo proposito, considerata la mole di tali siti terzi, declina espressamente ogni relativa responsabilità. L'utente dovrebbe verificare la <strong>privacy e cookie policy</strong> dei siti di terzi cui accede per conoscere le condizioni applicabili al trattamento dei dati personali poiché la <strong>privacy e cookie policy</strong> del sito <strong><?php print render($website); ?></strong> si applica solo al sito come sopra definito.
+        </p>
+        <h3>Cookie persistenti</h3>
+        <p>
+          I cookies persistenti sono memorizzati sul dispositivo degli utenti tra le sessioni del browser, consentendo di ricordare le preferenze o le azioni dell'utente in un sito. Possono essere utilizzati per diversi scopi, ad esempio per ricordare le preferenze e le scelte quando si utilizza il sito.
+        </p>
+        <h3>Cookie essenziali</h3>
+        <p>
+          Questi cookies sono strettamente necessari per il funzionamento del sito. Senza l'uso di tali cookie alcune parti del sito non funzionerebbero. Comprendono, ad esempio, i cookie che consentono di accedere in aree protette del sito. Questi cookie non raccolgono informazioni per scopi di marketing e non possono essere disattivati.
+        </p>
+        <h3>Cookie funzionali</h3>
+        <p>
+          Questi cookie servono a riconoscere un utente che torna a visitare il sito. Permettono di personalizzare i contenuti e ricordare le preferenze (ad esempio, la lingua selezionata o la regione). Questi cookies non raccolgono informazioni che possono identificare l'utente. Tutte le informazioni raccolte sono anonime.
+        </p>
+
+        <?php /*
+        <h3>Cookie di condivisone sui Social Network</h3>
+        <p>
+          Questi cookie facilitano la condivisione dei contenuti del sito attraverso social network quali Facebook, Twitter, Google Plus. Per prendere visione delle rispettive privacy e cookie policies è possibile visitare i siti web dei social networks.
+        </p>
+        <ul>
+          <li>
+            Facebook: <a href="https://www.facebook.com/help/cookies" target="_blank">www.facebook.com/help/cookies</a>
+          </li>
+        </ul> */ ?>
+
+        <h2>Come rimuovere e/o modificare le impostazioni sui cookie</h2>
+        <p>
+          La maggior parte dei browser accetta automaticamente i cookie, ma l'utente normalmente può modificare le impostazioni per disabilitare tale funzione. È possibile bloccare tutte le tipologie di cookie, oppure accettare di riceverne soltanto alcuni e disabilitarne altri. La sezione Opzioni" o "Preferenze" nel menu del browser permettono di evitare di ricevere cookie e altre tecnologie di tracciamento utente, e come ottenere notifica dal browser dell'attivazione di queste tecnologie. In alternativa, è anche possibile consultare la sezione "Aiuto" della barra degli strumenti presente nella maggior parte dei browser.
+        </p>
+        <p>
+          Di seguito sono riportate le guide per la gestione dei cookie dei differenti browser:
+          <a href="http://windows.microsoft.com/it-it/windows7/how-to-manage-cookies-in-internet-explorer-9" rel="alternate" target="_blank">Internet Explorer</a>
+          -
+          <a href="https://support.google.com/chrome/answer/95647" rel="alternate" target="_blank">Chrome</a>
+          -
+          <a href="https://support.apple.com/it-it/HT201265" rel="alternate" target="_blank">Safari</a>
+          -
+          <a href="https://support.mozilla.org/it/kb/Attivare%20e%20disattivare%20i%20cookie" rel="alternate" target="_blank">Firefox</a>
+          -
+          <a href="http://www.opera.com/help/tutorials/security/" rel="alternate" target="_blank">Opera</a>.
+          <br />
+          Da dispositivo mobile:
+          <a href="https://support.google.com/chrome/answer/95647?hl=it" rel="alternate" target="_blank">Android</a>
+          -
+          <a href="https://support.apple.com/it-it/HT201265" rel="alternate" target="_blank">Safari</a>
+          -
+          <a href="http://www.windowsphone.com/it-it/how-to/wp7/web/changing-privacy-and-other-browser-settings" rel="alternate" target="_blank">Windows Phone</a>
+          -
+          <a href="http://docs.blackberry.com/en/smartphone_users/deliverables/32004/Turn_off_cookies_in_the_browser_60_1072866_11.jsp" rel="alternate" target="_blank">Blackberry</a>.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
